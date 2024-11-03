3ed3d8ef36b1b05e80e895a3e432cc143e70c6e7
Nazila Faizzadah <nazilafaizzadah37@gmail.com>
2024-11-03 11:34:03 -0800

    change token validation function

--- src/components/useTokenCheck.jsx
+++ src/components/useTokenCheck.jsx
@@ -1,23 +1,61 @@
+// import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { jwtDecode } from 'jwt-decode';
+
+// const useTokenCheck = () => {
+//     const navigate = useNavigate(); 
+//     useEffect(() => {
+//         const tokenCheckInterval = setInterval(() => {
+//             const token = localStorage.getItem('token');
+
+//             if (token) {
+//                 const decodedToken = jwtDecode(token);
+//                 const currentTime = Date.now() / 1000;
+//                 if (decodedToken.exp < currentTime) {
+//                     localStorage.removeItem('token');
+//                     navigate('/login'); 
+//                 }
+//             }
+//         }, 1000); 
+//         return () => clearInterval(tokenCheckInterval);
+//     }, [navigate]);
+// };
+
+// export default useTokenCheck;
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 
 const useTokenCheck = () => {
-    const navigate = useNavigate(); 
-    useEffect(() => {
-        const tokenCheckInterval = setInterval(() => {
-            const token = localStorage.getItem('token');
+    const navigate = useNavigate();
 
-            if (token) {
-                const decodedToken = jwtDecode(token);
-                const currentTime = Date.now() / 1000;
-                if (decodedToken.exp < currentTime) {
-                    localStorage.removeItem('token');
-                    navigate('/login'); 
-                }
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        
+        if (token) {
+            const decodedToken = jwtDecode(token);
+            const currentTime = Date.now() / 1000;
+
+            // Initial check for expiration
+            if (decodedToken.exp < currentTime) {
+                localStorage.removeItem('token');
+                navigate('/login');
+            } else {
+                // Set up interval to check token validity every 5 minutes
+                const tokenCheckInterval = setInterval(() => {
+                    const updatedToken = localStorage.getItem('token');
+                    if (updatedToken) {
+                        const decodedUpdatedToken = jwtDecode(updatedToken);
+                        if (decodedUpdatedToken.exp < currentTime) {
+                            localStorage.removeItem('token');
+                            navigate('/login');
+                        }
+                    }
+                }, 300000); // 5 minutes
+
+                return () => clearInterval(tokenCheckInterval);
             }
-        }, 1000); 
-        return () => clearInterval(tokenCheckInterval);
+        }
     }, [navigate]);
 };
 
